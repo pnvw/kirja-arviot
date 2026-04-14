42be9f115e6445608fcbcaa00a175ef99668e768
Priscilla <priscilla.viitanen@gmail.com>
2026-04-14 17:27:49 +0300

    Add not found handling

--- items.py
+++ items.py
@@ -21,7 +21,8 @@ def get_item(item_id):
              FROM items, users
              WHERE items.user_id = users.id AND
                    items.id = ?"""
-    return db.query(sql, [item_id])[0]
+    result = db.query(sql, [item_id])
+    return result[0] if result else None
 
 def update_item(item_id, book_type, book_name, writer, review, grade):
     sql = """UPDATE items SET book_type = ?,
